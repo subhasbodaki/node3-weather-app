9aac68086f8096021dd42421f80b45456c644045
subhasbodaki <sbodakiyavar@gmail.com>
2021-03-30 18:14:18 +0530

    Removed unnecessary conslole.log calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('This is client side javascript')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message1')
